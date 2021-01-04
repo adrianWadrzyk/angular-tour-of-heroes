705d366ea3bd7487430e6e5c590247c0716b014b
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-01-04 17:52:59 +0100

    init and import message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service'; 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  constructor() { }
+
+  heroes: Hero[];
+  constructor(private HeroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   selectedHero:Hero;
 
@@ -19,4 +21,7 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
   
+  getHeroes():void { 
+    this.HeroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
