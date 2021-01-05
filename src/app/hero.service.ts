0b30f6adcd7690cf5cce3c36aa493fb5baef3582
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-01-05 16:50:58 +0100

    add updateHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
   private heroesUrl:string = 'api/heroes';
+  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient,
               private messageService : MessageService) { }
@@ -23,11 +27,14 @@ getHeroes(): Observable<Hero[]> {
     );
 }
   
-  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
-  }
-  
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
+}
+
   private handleError<T>(operation = 'operation', result?: T) { 
     return (error: any): Observable<T> => { 
       console.log(error);
@@ -39,5 +46,12 @@ getHeroes(): Observable<Hero[]> {
 
   private log(message: string) { 
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
