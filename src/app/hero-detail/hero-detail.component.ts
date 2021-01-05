87eeace4ee75af248b44a1c49945f9c32808b470
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-01-05 16:47:29 +0100

    add save button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -26,7 +26,14 @@ export class HeroDetailComponent implements OnInit {
     .subscribe(hero => this.hero = hero);
     }
 
-    goBack(): void { 
-      this.location.back();
-    }
+  goBack(): void { 
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe( () => this.goBack());
+  }
+
+
 }
