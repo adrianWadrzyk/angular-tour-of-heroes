751ce8c4b35d4afbe356da1eeb9c5b69ba2469c8
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-01-06 15:39:00 +0100

    add search by name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -55,6 +55,18 @@ getHero(id: number): Observable<Hero> {
     );
   }
 
+searchHeroes(term: string): Observable<Hero[]> {
+  if (!term.trim()) {
+    return of([]);
+  }
+  return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+    tap(x => x.length ?
+       this.log(`found heroes matching "${term}"`) :
+       this.log(`no heroes matching "${term}"`)),
+    catchError(this.handleError<Hero[]>('searchHeroes', []))
+  );
+}
+
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
     tap((newHero: Hero) => this.log(`added hero w/id=${newHero.id}`)),
